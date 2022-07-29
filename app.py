e39b6f1fd2525c1178d9eec13070adba2a501ac3
Mario Pérez <48880789+mapecode@users.noreply.github.com>
2022-07-29 17:07:10 +0200

    remove intro text

--- app.py
+++ app.py
@@ -3,14 +3,7 @@ import streamlit as st
 
 from utils import load_index_data, file_download, price_plot
 
-st.title('S&P 500 App')
-
-st.markdown("""
-This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
-* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
-* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
-""")
-
+st.title('S&P 500 Index App')
 st.sidebar.header('User Input Features')
 
 # Web scraping of S&P 500 data
